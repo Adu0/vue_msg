e1308e45cd36416b5d8d2377d8a89e4bdb470632
cole <591212363@qq.com>
2018-11-22 13:00:49 +0800

    完成了首页,和新闻详情页

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app-container">
+      <mt-header fixed title="固定在顶部"></mt-header> 
+       
+  </div>
+</template>
+
+<script>
+// export default {
+//   name: 'App'
+// }
+</script>
+
+<style>
+
+</style>
